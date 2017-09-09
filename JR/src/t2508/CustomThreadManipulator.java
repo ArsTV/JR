290388830ddbb3c2f79e8d9cd36a43b9e5881aeb
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-08 21:35:13 -0400

    create TaskManipulator logic:
    1. TaskManipulator have to implement Runnable and
    CustomThreadManipulator
    2. run method have to print current thread name every 100 msec
    3. stop method have to interrupt last created thread

---
+++ JR/src/t2508/CustomThreadManipulator.java
@@ -0,0 +1,6 @@
+package t2508;
+
+public interface CustomThreadManipulator {
+    public void start(String threadName);
+    public void stop();
+}
---
+++ JR/src/t2508/TaskManipulator.java
@@ -0,0 +1,9 @@
+package t2508;
+
+public class TaskManipulator implements Runnable {
+    private Thread thread;
+    @Override
+    public void run() {
+    }
+
+}
