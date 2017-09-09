c1002bba23ac1912838d30a1875dff23daf95817
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-08 21:37:22 -0400

    Created logics of TaskManipulator class

--- JR/src/t2508/TaskManipulator.java
+++ JR/src/t2508/TaskManipulator.java
@@ -1,9 +1,31 @@
 package t2508;
 
-public class TaskManipulator implements Runnable {
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
     private Thread thread;
     @Override
     public void run() {
+        while(!thread.isInterrupted()){
+            try {
+                while (!thread.isInterrupted()) {
+                    Thread.sleep(0);
+                    System.out.println(Thread.currentThread().getName());
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
+    @Override
+    public void start(String threadName) {
+        this.thread = new Thread(this);
+        this.thread.setName(threadName);
+        this.thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
 }
