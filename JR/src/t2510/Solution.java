eb3fa29f4e2b8cfad3e3a1950a7e84d7268c939c
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-10 22:26:49 -0400

    caught thread exceptions by UncaughtExceptionHandler

--- JR/src/t2510/Solution.java
+++ JR/src/t2510/Solution.java
@@ -1,5 +1,25 @@
 package t2510;
 
-public class Solution {
+public class Solution extends Thread {
+    Thread.UncaughtExceptionHandler uncaughtExceptionHandler = new ExceptionHandler();
 
+    public Solution() {
+       
+    }
+    public static class ExceptionHandler implements UncaughtExceptionHandler{
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            if(e instanceof Error){
+                System.out.println("Error");
+            } else if(e instanceof Exception){
+                System.out.println("Exception");
+            } else {
+                System.out.println("Throwable");
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+    }
 }
