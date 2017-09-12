1e9a86b1c07c4dd14bb7ffa0ef4632d3e43612e0
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-11 22:46:29 -0400

    uncaughtException print how it should:
    java.lang.IllegalAccessException: GHI
    java.lang.RuntimeException: DEF
    java.lang.Exception: ABC

--- JR/src/t2512/Solution.java
+++ JR/src/t2512/Solution.java
@@ -1,9 +1,23 @@
 package t2512;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+    	t.interrupt();
+        List<Throwable> list = new ArrayList<>();
+
+        do {
+            list.add(e);
+            e = e.getCause();
+        } while (e != null);
+
+        for (int i = list.size()-1; i >= 0; i--) {
+            System.out.println(list.get(i).fillInStackTrace());
+        }
     }
 
     public static void main(String[] args) {
