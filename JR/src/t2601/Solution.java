d5ee9542f1b7651652f6f3f582a23c0faecd4953
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-17 23:24:43 -0400

    Have to make method sort who search median and return array (sorted
    by increasing distance from the median)

---
+++ JR/src/t2601/Solution.java
@@ -0,0 +1,13 @@
+package t2601;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer[] sort(Integer[] array) {
+    	return null;    	
+    }
+}
