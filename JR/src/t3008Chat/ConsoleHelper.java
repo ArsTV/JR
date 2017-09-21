599d10a67c1ce3a4bb193c441d875d5779cff11b
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-20 21:30:49 -0400

    realized ConsoleHelper class

--- JR/src/t3008Chat/ConsoleHelper.java
+++ JR/src/t3008Chat/ConsoleHelper.java
@@ -1,5 +1,43 @@
 package t3008Chat;
 
-public class ConsoleHelper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String line = null;
+
+        while(true) {
+            try {
+                line = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+
+        return line;
+    }
+
+    public static int readInt(){
+        int i = 0;
+
+        while (true) {
+            try {
+                i = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+
+        return i;
+    }
 }
