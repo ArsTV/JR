ff7de5199dc410700127a307e55671534e409176
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-21 23:15:10 -0400

    Counted even and pdd quantity

--- JR/src/t0606/Solution.java
+++ JR/src/t0606/Solution.java
@@ -1,11 +1,25 @@
 package t0606;
 
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Solution {
-	public static int even;
+
+    public static int even;
     public static int odd;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        Scanner scanner = new Scanner(System.in);
+        String [] line = scanner.nextLine().split("");
+        for (int i = 0; i < line.length; i++) {
+            if((Integer.parseInt(line[i]) %2) == 0){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
     }
-
 }
