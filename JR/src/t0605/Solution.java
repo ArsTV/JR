3c5795ecb6be742fb7e6c3a4431e865d72f04025
DELL <DELL@DELL-Laptop-M5A6LK>
2017-09-21 23:59:28 -0400

    Realized Body class

--- JR/src/t0605/Solution.java
+++ JR/src/t0605/Solution.java
@@ -14,7 +14,16 @@ public class Solution {
 
     public static class Body {
         public static void massIndex(double weight, double height) {
-            
+        	double d = weight / (height * height);
+
+            if(d < 18.5)
+                System.out.println("Underweight: less than 18.5");
+            else if(d >= 18.5 && d <= 24.9)
+                System.out.println("Normal: between 18.5 and 24.9");
+            else if(d >= 25 && d <= 29.9)
+                System.out.println("Overweight: between 25 and 29.9");
+            else if(d >= 30)
+                System.out.println("Obesity: 30 or more");            
         }
     }
 }
