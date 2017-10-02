09c86db84ce7d1f66706224d56387ddac4efce6d
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-01 21:09:58 -0400

    Have to create list of names with salary.
    And remove with salary < 500

---
+++ JR/src/t0818/Solution.java
@@ -0,0 +1,17 @@
+package t0818;
+
+import java.util.*;
+
+public class Solution {
+    public static HashMap<String, Integer> createMap() {
+        
+    }
+
+    public static void removeItemFromMap(HashMap<String, Integer> map) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
