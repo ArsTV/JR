2a63e86149252456d285b3e53237d96fc2acfaf0
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-01 22:36:57 -0400

    Have to realize method log:
    print class name and method name who called log method 
    + String s

---
+++ JR/src/t0906/Solution.java
@@ -0,0 +1,11 @@
+package t0906;
+
+public class Solution {
+    public static void main(String[] args) {
+        log("In main method");
+    }
+
+    public static void log(String s) {
+      
+    }
+}
