50c1691ec00c0db52c7f3c406abd534d8b853d79
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-01 21:13:45 -0400

    Created map and remove method

--- JR/src/t0818/Solution.java
+++ JR/src/t0818/Solution.java
@@ -4,11 +4,28 @@ import java.util.*;
 
 public class Solution {
     public static HashMap<String, Integer> createMap() {
-        
+        HashMap<String, Integer> list = new HashMap<>();
+        list.put("A", 1);
+        list.put("B", 1000);
+        list.put("C", 150);
+        list.put("D", 1);
+        list.put("E", 1);
+        list.put("F", 1);
+        list.put("G", 19);
+        list.put("H", 1);
+        list.put("I", 1);
+        list.put("J", 1);
+        return list;
     }
 
     public static void removeItemFromMap(HashMap<String, Integer> map) {
-        
+        HashMap<String, Integer> copy = new HashMap<>(map);
+        for(Map.Entry<String, Integer> entry: copy.entrySet()){
+            if(entry.getValue() < 500){
+                map.remove(entry.getKey());
+            }
+        }
+
     }
 
     public static void main(String[] args) {
