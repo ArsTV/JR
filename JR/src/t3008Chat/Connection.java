ffa1d248f941bd349759d683c09fca522bd6327d
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-25 22:31:33 -0400

    Made logic of Connection class.
    Connection by Socket and send , receive message.
    realized interface Closeable.

--- JR/src/t3008Chat/Connection.java
+++ JR/src/t3008Chat/Connection.java
@@ -1,5 +1,42 @@
 package t3008Chat;
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
 
-public class Connection {
+/**
+ * Created by DELL on 9/19/2017.
+ */
+public class Connection implements Closeable{
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
 
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    void send (Message message) throws IOException{
+        synchronized (out){
+            out.writeObject(message);
+        }
+    }
+
+    Message receive() throws IOException, ClassNotFoundException{
+        synchronized(in){
+            return (Message)in.readObject();
+        }
+    }
+
+    SocketAddress getRemoteSocketAddress(){
+        return socket.getRemoteSocketAddress();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
