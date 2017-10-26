0107e567bcd1e18ce9b2ffe8770a9e5f346b12d4
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-26 00:08:35 -0400

    Made part of Server class logic

--- JR/src/t3008Chat/Server.java
+++ JR/src/t3008Chat/Server.java
@@ -1,10 +1,34 @@
 package t3008Chat;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by DELL on 9/19/2017.
+ */
 public class Server {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args){
+        ConsoleHelper.writeMessage("Type socket port.");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt());){
+            ConsoleHelper.writeMessage("Server started.");
+            for(;;){
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Srver error.");
 
-	}
+        }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        Handler(Socket socket){
+            this.socket = socket;
+        }
+
+    }
 
 }
