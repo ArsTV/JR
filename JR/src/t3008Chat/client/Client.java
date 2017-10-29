40ca71ffed2cd7d21a756ff8776fa20d2d02c859
DELL <DELL@DELL-Laptop-M5A6LK>
2017-10-29 12:18:52 -0400

    Have to create Client class with 2 threads
    first thread will read messages from console and will send it to the
    server
    second thread will receive messages from the server and print it in
    console

---
+++ JR/src/t3008Chat/client/Client.java
@@ -0,0 +1,17 @@
+package t3008Chat.client;
+
+import t3008Chat.Connection;
+
+/**
+ * Created by DELL on 10/29/2017.
+ */
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+
+    public class SocketThread extends Thread{
+
+    }
+}
