f7ab250bb208e02e78176cea556cf88ea470021a
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-01 23:22:35 -0400

    Started to make own archiver

---
+++ JR/src/t3110/Archiver.java
@@ -0,0 +1,38 @@
+package t3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+/**
+ * Created by DELL on 11/1/2017.
+ */
+public class Archiver {
+    public static void main(String[] args) throws IOException, Exception {
+        System.out.println("Enter full path of the archive.");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String path = reader.readLine();
+
+        /*Pattern pattern = Pattern.compile("\\w+\\.zip");
+
+        Matcher matcher = pattern.matcher(path);
+        matcher.find();
+        String fileName = matcher.group();*/
+
+        Path path1 = Paths.get(path);
+
+        ZipFileManager zipFileManager = new ZipFileManager(path1);
+
+        Path path2 = Paths.get(reader.readLine());
+
+        zipFileManager.createZip(path2);
+
+        reader.close();
+
+
+
+    }
+}
---
+++ JR/src/t3110/ZipFileManager.java
@@ -0,0 +1,19 @@
+package t3110;
+
+
+import java.nio.file.Path;
+
+/**
+ * Created by DELL on 11/1/2017.
+ */
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception{
+
+    }
+}
