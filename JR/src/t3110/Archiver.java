b074dd78218ac16e637d7c94fb630a1af89dd59e
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-02 22:55:39 -0400

    Made method createZip who write zipFile

--- JR/src/t3110/Archiver.java
+++ JR/src/t3110/Archiver.java
@@ -16,19 +16,13 @@ public class Archiver {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String path = reader.readLine();
 
-        /*Pattern pattern = Pattern.compile("\\w+\\.zip");
-
-        Matcher matcher = pattern.matcher(path);
-        matcher.find();
-        String fileName = matcher.group();*/
-
         Path path1 = Paths.get(path);
 
         ZipFileManager zipFileManager = new ZipFileManager(path1);
 
-        Path path2 = Paths.get(reader.readLine());
+        //Path path2 = Paths.get(reader.readLine());
 
-        zipFileManager.createZip(path2);
+        zipFileManager.createZip(path1);
 
         reader.close();
 
--- JR/src/t3110/ZipFileManager.java
+++ JR/src/t3110/ZipFileManager.java
@@ -1,7 +1,10 @@
 package t3110;
 
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by DELL on 11/1/2017.
@@ -14,6 +17,25 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception{
+
+
+        try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(source))) {
+
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+
+            zipOutputStream.putNextEntry(zipEntry);
+
+           
+//****************
+            zipOutputStream.write(Files.readAllBytes(source));
+
+//****************
+            /*InputStream inputStream = Files.newInputStream(source.getFileName());
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                zipOutputStream.write(i);
+            }*/
+        }
 
     }
 }
