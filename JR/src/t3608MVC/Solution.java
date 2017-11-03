424ac61ea4a3eaffe0e2aa3f0fcfbb558f57bdaa
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-02 23:24:16 -0400

    mvc

--- JR/src/t3608MVC/Solution.java
+++ JR/src/t3608MVC/Solution.java
@@ -1,5 +1,32 @@
 package t3608MVC;
 
-public class Solution {
 
+import t3608MVC.controller.Controller;
+import t3608MVC.model.MainModel;
+import t3608MVC.model.Model;
+import t3608MVC.view.EditUserView;
+import t3608MVC.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        EditUserView editUserView = new EditUserView();
+
+        Controller controller = new Controller();
+        usersView.setController(controller);
+        editUserView.setController(controller);
+
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
+
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126);
+
+        editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged( "Ars", 126, 9);
+
+        usersView.fireEventShowDeletedUsers();
+    }
 }
