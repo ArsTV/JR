063c302941c10ff32191867a4cbea2d8bc324047
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-04 01:35:42 -0400

    In the class we return the corresponding method depending on the command

---
+++ JR/src/t3110/CommandExecutor.java
@@ -0,0 +1,36 @@
+package t3110;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import t3110.command.Command;
+import t3110.command.*;
+
+/**
+ * Created by DELL on 11/4/2017.
+ */
+public class CommandExecutor {
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
+    static {
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
+
+    private CommandExecutor() {
+    }
+
+    public static void execute(Operation operation) throws Exception {
+        //allKnownCommandsMap.get(operation).execute();
+
+        for(Map.Entry<Operation, Command> entry: allKnownCommandsMap.entrySet()){
+            if(operation.equals(entry.getKey())){
+                entry.getValue().execute();
+            }
+        }
+    }
+}
