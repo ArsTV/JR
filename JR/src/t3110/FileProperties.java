4e3f3af01cd954e8beb65b82ca6e54462c4d7846
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-05 02:27:30 -0500

    made FileProperties class for information about file compression in archive

---
+++ JR/src/t3110/FileProperties.java
@@ -0,0 +1,56 @@
+package t3110;
+
+/**
+ * Created by DELL on 11/5/2017.
+ */
+public class FileProperties  {
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public long getCompressionRatio(){
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name);
+
+        if(size > 0){
+            stringBuilder.append("\t");
+            stringBuilder.append(size/1024);
+            stringBuilder.append("\t Kb (");
+            stringBuilder.append(compressedSize/1024);
+            stringBuilder.append(" Kb) compressed: ");
+            stringBuilder.append(getCompressionRatio());
+            stringBuilder.append("%");
+        }
+
+        return stringBuilder.toString();
+    }
+}
