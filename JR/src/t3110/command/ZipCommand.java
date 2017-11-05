24d52d031b4fc4f9c69019bdc9ac98ce917fe1eb
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-05 01:36:48 -0500

    Realized  execute method in ZipCreateCommand class

--- JR/src/t3110/command/ZipCommand.java
+++ JR/src/t3110/command/ZipCommand.java
@@ -1,5 +1,17 @@
 package t3110.command;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import t3110.ConsoleHelper;
+import t3110.ZipFileManager;
+
 public abstract class ZipCommand implements Command{
 
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("You need to write full path");
+        String fullPathString = ConsoleHelper.readString();
+        Path fullPath = Paths.get(fullPathString);
+        return new ZipFileManager(fullPath);
+    }
 }
--- JR/src/t3110/command/ZipCreateCommand.java
+++ JR/src/t3110/command/ZipCreateCommand.java
@@ -1,5 +1,12 @@
 package t3110.command;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import t3110.ConsoleHelper;
+import t3110.ZipFileManager;
+import t3110.exception.PathIsNotFoundException;
+
 /**
  * Created by DELL on 11/3/2017.
  */
@@ -7,6 +14,19 @@ package t3110.command;
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Creating archive");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("You need to write full name of file or directory for archiving it.");
+            Path path = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.createZip(path);
+
+            ConsoleHelper.writeMessage("Archive created");
+        }catch (PathIsNotFoundException e){
+            ConsoleHelper.writeMessage("Wrong file name or directory name");
+        }
     }
 }
