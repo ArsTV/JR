cab91f4008a86ee66a778ec2676fbbb827758f6e
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-05 22:10:01 -0500

    Realized execute() method of the ZipContentCommand class.
    where we scan contents of the archive.

--- JR/src/t3110/command/ZipContentCommand.java
+++ JR/src/t3110/command/ZipContentCommand.java
@@ -1,8 +1,31 @@
 package t3110.command;
 
+import java.util.List;
+
+import t3110.ConsoleHelper;
+import t3110.FileProperties;
+import t3110.ZipFileManager;
+
+/**
+ * Created by DELL on 11/3/2017.
+ */
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Scan contents of the archive.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Contents of the archive:");
+
+        List<FileProperties> filePropertiesList = zipFileManager.getFilesList();
+
+        for(FileProperties f: filePropertiesList){
+            ConsoleHelper.writeMessage(f.toString());
+        }
+
+        ConsoleHelper.writeMessage("Contents of the archive was read.");
+
 
     }
 }
