5725682dcf746c7a751baf5d3282fd42c5e45b09
DELL <DELL@DELL-Laptop-M5A6LK>
2017-11-12 21:48:32 -0500

    created counter of priority with AtomicInteger

--- JR/src/t2805/MyThread.java
+++ JR/src/t2805/MyThread.java
@@ -1,5 +1,80 @@
 package t2805;
 
-public class MyThread {
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Created by DELL on 11/12/2017.
+ */
+public class MyThread extends Thread {
+    public static AtomicInteger priority = new AtomicInteger(1);
+    public MyThread() {
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(String name) {
+        super(name);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+        if(priority.get()<=10){
+            setPriority(priority.getAndIncrement());
+            if(priority.get() > 10)
+                priority.set(1);
+        }
+    }
 }
