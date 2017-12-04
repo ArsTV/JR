6733b372541847ef0546e1151f5ce0e607f75d6e
DELL <DELL@192.168.2.77>
2017-12-03 23:34:16 -0500

    Corrected the implementation error with double value to BigDecimal value

--- JR/src/t2907/Solution.java
+++ JR/src/t2907/Solution.java
@@ -8,7 +8,11 @@ public class Solution {
         System.out.println(getValue(1.1d, 1.2d));
     }
 
-    public static BigDecimal getValue(double v1, double v2) {
-        return new BigDecimal(v1).add(new BigDecimal(v2));
+    public static BigDecimal getValue(double v1, double v2) {Double d1 = v1;
+    Double d2 = v2;
+    return new BigDecimal(d1.toString()).add(new BigDecimal(d2.toString()));
+
+    //or using String.valueOf:
+    //return new BigDecimal(String.valueOf(v1)).add(new BigDecimal(String.valueOf(v2)));
     }
 }
