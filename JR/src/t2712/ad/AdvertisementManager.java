8490e67d2f15a831c00b90c05a646369198ddd7f
DELL <DELL@DELL-Laptop-M5A6LK>
2017-12-19 22:01:18 +0100

    made first test video storage in AdvertisementStorage class

--- JR/src/t2712/ad/AdvertisementManager.java
+++ JR/src/t2712/ad/AdvertisementManager.java
@@ -1,5 +1,5 @@
 package t2712.ad;
 
 public class AdvertisementManager {
-
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
--- JR/src/t2712/ad/AdvertisementStorage.java
+++ JR/src/t2712/ad/AdvertisementStorage.java
@@ -1,15 +1,31 @@
 package t2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by DELL on 12/19/2017.
  */
 public class AdvertisementStorage {
-    private static AdvertisementStorage advertisementStorage;
+    private static final AdvertisementStorage instance = new AdvertisementStorage();
+    private final List<Advertisement> videos = new ArrayList<>();
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); //duration 3 min
+        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //duration 15 min
+        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //duration 10 min
     }
 
-    public AdvertisementStorage getInstance() {
-        return advertisementStorage;
+    public static AdvertisementStorage getInstance() {
+        return instance;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
