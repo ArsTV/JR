16a0ccc5ef146a1a8d76218a475a8d50e23ad2c1
DELL <DELL@DELL-Laptop-M5A6LK>
2017-12-22 15:56:25 +0100

    Using java.util.concurrent.Phaser realize next logic:
    1. at first all characters have to connect to the game
    2. all characters preparing for playing
    3. they enter to the game
    4. and finally they all die together

---
+++ JR/src/t2809/Character.java
@@ -0,0 +1,26 @@
+package t2809;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class Character implements Runnable {
+    protected abstract int getId();
+
+    public void run() {
+        System.out.println(toString() + " entered to the game");
+        doSomething();
+        System.out.println(toString() + " died");
+    }
+
+    private void doSomething() {
+        try {
+            Thread.currentThread().sleep(ThreadLocalRandom.current().nextInt(10, 100));
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " #" + getId();
+    }
+
+}
