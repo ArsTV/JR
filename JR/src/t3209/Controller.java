a0fd8822387256eab89e33a71eaa2ef5c27c42fe
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-05 08:23:03 +0100

    In Controller class made getPlainText() method who g
    et the text from the document with all the html tags.

--- JR/src/t3209/Controller.java
+++ JR/src/t3209/Controller.java
@@ -7,6 +7,7 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
  * Created by DELL on 11/14/2017.
@@ -52,6 +53,19 @@ public class Controller {
         } catch (BadLocationException e) {
             ExceptionHandler.log(e);
         }
+    }
+    
+
+    public String getPlainText(){
+        StringWriter stringWriter = new StringWriter();
+        try {
+            new HTMLEditorKit().write(stringWriter, document, 0, document.getLength());
+        } catch (IOException e) {
+            ExceptionHandler.log(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
     }
 
     public void init(){
