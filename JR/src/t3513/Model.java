d204b4c6ed2c0d10748f960c46034f50ffcf65ea
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-12 15:20:58 +0100

    In Model class created array of Tiles for initializing the playing field
    and filling it with empty tiles

--- JR/src/t3513/Model.java
+++ JR/src/t3513/Model.java
@@ -1,5 +1,19 @@
 package t3513;
 
+/**
+ * Created by DELL on 1/12/2018.
+ */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
 
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
