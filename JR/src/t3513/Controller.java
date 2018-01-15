75672491d017d8061171edb271348142a69bcf54
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-15 15:14:25 +0100

    Realized View class  like JPanel
    In Controller class overrided keyPressed method

--- JR/src/t3513/Controller.java
+++ JR/src/t3513/Controller.java
@@ -1,6 +1,7 @@
 package t3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by DELL on 1/12/2018.
@@ -8,12 +9,57 @@ import java.awt.event.KeyAdapter;
 public class Controller extends KeyAdapter {
     Model model;
     View view;
+    public static final int WINNING_TILE = 2048;
 
-    public Tile[][] getGameTiles(){
+    public Controller(Model model) {
+        this.model = model;
+        view = new View(this);
+    }
+
+    public Tile[][] getGameTiles() {
         return model.getGameTiles();
     }
 
-    public int getScore(){
+    public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        } else {
+            if (!model.canMove()) {
+                view.isGameLost = true;
+            } else {
+                if (!view.isGameLost && !view.isGameWon) {
+                    switch (keyEvent.getKeyCode()) {
+                        case KeyEvent.VK_LEFT:
+                            model.left();
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            model.right();
+                            break;
+                        case KeyEvent.VK_UP:
+                            model.up();
+                            break;
+                        case KeyEvent.VK_DOWN:
+                            model.down();
+                            break;
+                    }
+                }
+                if (model.maxTile == WINNING_TILE) {
+                    view.isGameWon = true;
+                }
+            }
+        }
+        view.repaint();
     }
 }
