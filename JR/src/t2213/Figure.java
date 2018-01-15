5bacd844accefd329a8c730ba072943219bd1121
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-16 00:01:48 +0100

    In Figure class created matrix for showing a figure
    created methods of moving a figure

--- JR/src/t2213/Figure.java
+++ JR/src/t2213/Figure.java
@@ -1,5 +1,38 @@
 package t2213;
 
+/**
+ * Created by DELL on 1/15/2018.
+ */
 public class Figure {
+    private int x, y;
+    private int[][] matrix = new int[3][3];
 
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void left(){}
+    public void right(){}
+    public void up(){}
+    public void down(){}
+    public void rotate(){}
+    public void downMaximum(){}
+    public void landed(){}
+    public boolean isCurrentPositionAvailable(){
+        return true;
+    }
 }
