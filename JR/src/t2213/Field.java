d17765d44311b3877fe6ea4d4525405425e2b2b3
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-15 23:47:13 +0100

    In Field class created matrix of game field and  getter, setter and
    constructor to initialize it

--- JR/src/t2213/Field.java
+++ JR/src/t2213/Field.java
@@ -1,5 +1,39 @@
 package t2213;
 
+/**
+ * Created by DELL on 1/15/2018.
+ */
 public class Field {
+    private int width, height;
+    private int[][] matrix;
 
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new int[height][width];
+    }
+
+    public void print(){}
+
+    public void removeFullLines(){}
+
+    public Integer getValue(int x, int y){
+        return matrix[y][x];
+    }
+
+    public void setValue(int x, int y, int value){
+        matrix[y][x] = value;
+    }
 }
--- JR/src/t2213/Tetris.java
+++ JR/src/t2213/Tetris.java
@@ -1,12 +1,29 @@
 package t2213;
 
+/**
+ * Created by DELL on 1/15/2018.
+ */
 public class Tetris {
+
     private Field field;
     private Figure figure;
+    protected static Tetris game;
 
+    public Field getField() {
+        return field;
+    }
 
-	public static void main(String[] args) {
-		
-	}
+    public Figure getFigure() {
+        return figure;
+    }
 
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+        
+    }
+
+    public void run(){}
+
+    public void step(){}
 }
