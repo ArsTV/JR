0a6c821fb0dc2e39469b0d23da4668a9802eb16a
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-22 12:14:52 +0100

    It need to realize the logic of the getFileTree method,
     which should find a list of all files including nested ones in the root
    directory.
    
    Use the queue, do not use recursion.
    
    Return a list of all paths to the files found, no need to return the
    path to the directories.
    The path must be absolute.

---
+++ JR/src/t3102/Solution.java
@@ -0,0 +1,19 @@
+package t3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/* 
+Find all files
+*/
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
