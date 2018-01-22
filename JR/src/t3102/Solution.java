fdfc82413dfa7622a211b48d84731506fdfe4a72
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-22 12:16:37 +0100

    Find all files in the root directory using Queue

--- JR/src/t3102/Solution.java
+++ JR/src/t3102/Solution.java
@@ -10,10 +10,31 @@ Find all files
 public class Solution {
     public static List<String> getFileTree(String root) throws IOException {
 
-        return null;
+        File file = new File(root);
+        List<String> dirList = new ArrayList<>();
+        Queue<File> fileQueue = new LinkedList<>();
+
+        Collections.addAll(fileQueue, file.listFiles());
+
+        while (!fileQueue.isEmpty()) {
+            File fileTemp = fileQueue.remove();
+
+            if (fileTemp.isDirectory()) {
+                Collections.addAll(fileQueue, fileTemp.listFiles());
+            } else {
+                dirList.add(fileTemp.getAbsolutePath());
+            }
+        }
+
+        return dirList;
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<String> list = getFileTree("C:/Test");
+
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
