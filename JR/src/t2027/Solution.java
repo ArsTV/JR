5090fe46c38f8e7d0e0cf854b8d50000b02e481d
DELL <DELL@DELL-Laptop-M5A6LK>
2018-01-22 11:00:48 +0100

    Realized the task of passing through the file tree and sorting files
    in  the path directory and in all its subdirectories

--- JR/src/t2027/Solution.java
+++ JR/src/t2027/Solution.java
@@ -1,5 +1,68 @@
 package t2027;
 
-public class Solution {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class Solution {
+    private static List<File> fileList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        File path = new File(args[0]);
+        File resultFileAbsolutePath = new File(args[1]);
+        File allFilesContent = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
+
+        FileUtils.renameFile(resultFileAbsolutePath, allFilesContent);
+
+        try(FileOutputStream fos = new FileOutputStream(allFilesContent)){
+            fileTree(path);
+            fileList.sort(new FileCompare());
+
+            FileInputStream fis = null;
+            for(File f: fileList){
+                fis = new FileInputStream(f);
+
+                while(fis.available() > 0 ){
+                    fos.write(fis.read());
+                }
+                fos.write(System.lineSeparator().getBytes());
+
+            }
+            fos.flush();
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void fileTree(File file){
+        File[] files = file.listFiles();
+
+        for(File f: files){
+            if(f.isDirectory()){
+                fileTree(f);
+                continue;
+            }
+
+            if (f.length() > 50 )
+                FileUtils.deleteFile(f);
+            else
+                fileList.add(f);
+        }
+    }
+
+}
+
+class FileCompare implements Comparator<File>{
+
+    @Override
+    public int compare(File o1, File o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
